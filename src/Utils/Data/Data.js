53384b1b14eddd457f3e2a786ecf98935df6c48c
no0ne <sumit2011kmr@gmail.com>
2024-10-16 23:37:14 +0530

    update

--- src/Utils/Data/Data.js
+++ src/Utils/Data/Data.js
@@ -3,7 +3,7 @@ export const Datas = [
         "id": 1,
         "name": 'Sumit Kumar',
         "initials": "sk",
-        "image": require('../Images/1.png'),
+        "image": require('../Images/bg.jpeg'),
         "link" : 'https://sumit2011.github.io',
         "instagram" : "https://www.instagram.com/_su__mit___",
         "twitter" : 'https://twitter.com/_su__mit___',
