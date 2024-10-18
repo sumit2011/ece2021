ce5ad2015656d2efe4d870bf038b67650031d62d
no0ne <sumit2011kmr@gmail.com>
2024-10-18 16:25:04 +0530

    update

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -4,19 +4,7 @@ import styled from 'styled-components';
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 
-// Define the CustomImage interface
-export interface CustomImage {
-  src: string;
-  original: string;
-  width: number;
-  height: number;
-  alt?: string;
-  tags?: { value: string; title: string }[];
-  caption?: string;
-}
-
-// Sample images array
-const images: CustomImage[] = [
+const images = [
   {
     src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
     original: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
@@ -28,6 +16,7 @@ const images: CustomImage[] = [
     ],
     caption: "After Rain (Jeshu John - designerspics.com)",
   },
+  // Add more images as needed
   {
     src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
     original: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
@@ -86,140 +75,25 @@ const images: CustomImage[] = [
     height: 213,
     caption: "201H (gratisography.com)",
   },
-  {
-    src: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
-    original: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
-    alt: "Big Ben - London",
-    width: 248,
-    height: 320,
-    caption: "Big Ben (Tom Eversley - isorepublic.com)",
-  },
-  {
-    src: "https://c7.staticflickr.com/9/8785/28687743710_3580fcb5f0_b.jpg",
-    original: "https://c7.staticflickr.com/9/8785/28687743710_3580fcb5f0_b.jpg",
-    alt: "Red Zone - Paris",
-    width: 320,
-    height: 113,
-    tags: [{ value: "People", title: "People" }],
-    caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)",
-  },
-  {
-    src: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg",
-    original: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg",
-    alt: "Wood Glass",
-    width: 313,
-    height: 320,
-    caption: "Wood Glass (Tom Eversley - isorepublic.com)",
-  },
-  {
-    src: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
-    original: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
-    width: 320,
-    height: 213,
-    caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)",
-  },
-  {
-    src: "https://c4.staticflickr.com/9/8562/28897228731_ff4447ef5f_b.jpg",
-    original: "https://c4.staticflickr.com/9/8562/28897228731_ff4447ef5f_b.jpg",
-    width: 320,
-    height: 194,
-    caption: "Old Barn (Tom Eversley - isorepublic.com)",
-  },
-  {
-    src: "https://c2.staticflickr.com/8/7577/28973580825_d8f541ba3f_b.jpg",
-    original: "https://c2.staticflickr.com/8/7577/28973580825_d8f541ba3f_b.jpg",
-    alt: "Cosmos Flower",
-    width: 320,
-    height: 213,
-    caption: "Cosmos Flower Macro (Tom Eversley - isorepublic.com)",
-  },
-  {
-    src: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
-    original: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
-    width: 271,
-    height: 320,
-    caption: "Orange Macro (Tom Eversley - isorepublic.com)",
-  },
-  {
-    src: "https://c1.staticflickr.com/9/8330/28941240416_71d2a7af8e_b.jpg",
-    original: "https://c1.staticflickr.com/9/8330/28941240416_71d2a7af8e_b.jpg",
-    width: 320,
-    height: 213,
-    tags: [
-      { value: "Nature", title: "Nature" },
-      { value: "People", title: "People" },
-    ],
-    caption: "Surfer Sunset (Tom Eversley - isorepublic.com)",
-  },
-  {
-    src: "https://c1.staticflickr.com/9/8707/28868704912_cba5c6600e_b.jpg",
-    original: "https://c1.staticflickr.com/9/8707/28868704912_cba5c6600e_b.jpg",
-    width: 320,
-    height: 213,
-    tags: [
-      { value: "People", title: "People" },
-      { value: "Sport", title: "Sport" },
-    ],
-    caption: "Man on BMX (Tom Eversley - isorepublic.com)",
-  },
-  {
-    src: "https://c4.staticflickr.com/9/8578/28357117603_97a8233cf5_b.jpg",
-    original: "https://c4.staticflickr.com/9/8578/28357117603_97a8233cf5_b.jpg",
-    width: 320,
-    height: 213,
-    caption: "Ropeman - Thailand (Tom Eversley - isorepublic.com)",
-  },
-  {
-    src: "https://c4.staticflickr.com/8/7476/28973628875_069e938525_b.jpg",
-    original: "https://c4.staticflickr.com/8/7476/28973628875_069e938525_b.jpg",
-    width: 320,
-    height: 213,
-    caption: "Time to Think (Tom Eversley - isorepublic.com)",
-  },
-  {
-    src: "https://c6.staticflickr.com/9/8593/28357129133_f04c73bf1e_b.jpg",
-    original: "https://c6.staticflickr.com/9/8593/28357129133_f04c73bf1e_b.jpg",
-    width: 320,
-    height: 179,
-    tags: [
-      { value: "Nature", title: "Nature" },
-      { value: "Fauna", title: "Fauna" },
-    ],
-    caption: "Untitled (Jan Vasek - jeshoots.com)",
-  },
-  {
-    src: "https://c6.staticflickr.com/9/8893/28897116141_641b88e342_b.jpg",
-    original: "https://c6.staticflickr.com/9/8893/28897116141_641b88e342_b.jpg",
-    width: 320,
-    height: 215,
-    tags: [{ value: "People", title: "People" }],
-    caption: "Untitled (moveast.me)",
-  },
-  {
-    src: "https://c1.staticflickr.com/9/8056/28354485944_148d6a5fc1_b.jpg",
-    original: "https://c1.staticflickr.com/9/8056/28354485944_148d6a5fc1_b.jpg",
-    width: 257,
-    height: 320,
-    caption: "A photo by 贝莉儿 NG. (unsplash.com)",
-  },
-  {
-    src: "https://c7.staticflickr.com/9/8824/28868764222_19f3b30773_b.jpg",
-    original: "https://c7.staticflickr.com/9/8824/28868764222_19f3b30773_b.jpg",
-    width: 226,
-    height: 320,
-    caption: "A photo by Matthew Wiebe. (unsplash.com)",
-  },
-  // Add more images as needed
 ];
 
-const Container = styled.div`
-margin: 20px;
- margin-top: 150px;
- color: white;
- text-align: center;
 
+const Title = styled.div`
+margin-top: 110px;
+color: white;
+text-align: center;
 `;
 
+const Container = styled.div`
+  margin: 10px;
+  background-color: #152515;
+  // color: #00ff00;
+  padding: 10px;
+  box-shadow: 0 0 15px rgba(0, 255, 0, 0.3);
+  border-radius: 15px;
+
+
+`;
 
 const slides = images.map(({ original, width, height }) => ({
   src: original,
@@ -230,28 +104,31 @@ const slides = images.map(({ original, width, height }) => ({
 const GalleryImages = () => {
   const [index, setIndex] = useState(-1);
 
-  const handleClick = (index: number, item: CustomImage) => setIndex(index);
-
+  const handleClick = (index, item) => setIndex(index);
 
   return (
-    <Container>
-      {/* Render the image gallery */}
+    <div >
+      <Title>
       <h1>Gallery</h1>
+      </Title>
+      
+    <Container>
+      
       <Gallery
         images={images}
         onClick={handleClick}
         enableImageSelection={false}
-        margin={5} // Adjust spacing between images if needed
+        margin={5} 
       />
-
       <Lightbox
         slides={slides}
         open={index >= 0}
         index={index}
         close={() => setIndex(-1)}
       />
-
     </Container>
+    </div>
+    
   );
 };
 
