497101656efe45bf561af020c634e2b19d071f1c
no0ne <sumit2011kmr@gmail.com>
2024-10-24 18:35:59 +0530

    updates

--- src/Utils/Data/Confession.js
+++ src/Utils/Data/Confession.js
@@ -1,24 +1,17 @@
 export const Confessions = [
     {
         "id" : 1,
-        "name" : "anonymous",
-        "date" : "20-10-24",
-        "icon" : "🙄",
-        "confession" : "it's a sample confession...."
-    },
-    {
-        "id" : 2,
         "name" : "Shivam",
         "date" : "20-10-24",
         "icon" : "🤫",
         "confession" : "Ladka ho ya Ladki sabka katega !!"
     },
     {
-        "id" : 3,
+        "id" : 2,
         "name" : "turulob",
         "date" : "24-10-24",
         "icon" : "🫣",
-        "confession" : "Par katata to ladko ka hai ladkiyon ka Nahi😂...Ladke hote hin chutiye hain😂😂."
+        "confession" : "Par katata to bas ladko ka hai😂...Ladke hote hin chutiye hain😂😂."
     },
 
 
